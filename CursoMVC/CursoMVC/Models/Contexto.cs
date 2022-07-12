3355d01d9a61c6b0c73c2a1072e28f85870509fd
Bruno da Silva Pereira <dasilvapereira.bruno@gmail.com>
2022-07-12 14:49:57 -0300

    Projeto MVC/API

---
+++ CursoMVC/CursoMVC/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Contexto: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=localhost\SQLEXPRESS;Database=CursoMVC;Trusted_Connection=True");
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ CursoMVC/CursoMVC/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo deverá ser preenchido!")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "O campo deverá ser preenchido!")]
+        [Range(1,10,ErrorMessage ="Valor fora da faixa!!!")]
+        public int Quantidade { get; set; }
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
